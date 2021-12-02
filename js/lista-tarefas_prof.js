1f9c050e57f3fb98ccc8746eb856e337068384de
gustavomes <gustavomes@yahoo.com.br>
2021-12-02 03:00:42 -0300

    sem sono - mudança para o modelo do professor

---
+++ js/lista-tarefas_prof.js
@@ -0,0 +1,99 @@
+//Capturando as variáveis que serão manipuladas
+const btnAddTarefa = document.getElementById('adicionar-tarefa');
+const inputText = document.getElementById('nova-tarefa');
+const tarefasPendentes = document.querySelector('.tarefas-pendentes');
+const tarefasConcluidas = document.querySelector('.tarefas-concluidas');
+const data = document.getElementById('dataFinal');
+
+let dataIni = document.getElementById('dataInicial');
+let data2 = new Date();
+let dia = data2.getDate();
+let mes = data2.getMonth() + 1;
+let ano = data2.getFullYear();
+let dataCorrente = dia + "/" + mes + "/" + ano;
+console.log(dataCorrente);
+dataIni.innerText = `Data: ${dataCorrente}`;
+if(dia<10){
+    dia='0'+dia
+  } 
+  if(mes<10){
+    mes='0'+mes
+  } 
+today = ano+'-'+mes+'-'+dia;
+document.getElementById("dataFinal").setAttribute("min", today);
+
+//Define o indice da tarefa
+let contador = 1;
+
+//Criando o card que será exibido - Recebe o elemento pai (tarefasPendentes)
+let criaCard = (elementoPai) => {
+
+    if (inputText.value == '') {
+        alert('Digite uma tarefa');
+
+    } else if (dataFinal.value == '') {
+            alert('Digite uma data');
+    } else {
+    elementoPai.innerHTML += `<li class="tarefa">
+                                       
+                                        <div class="not-done"></div>
+                                        <div class="descripcion">
+                                            <p class="nome"> ${contador} - Tarefa: ${inputText.value} - Início: ${dataCorrente} - Término: ${data.value}</p>
+                                            <img class="excluir" src="assets/trash_icon.png" alt="Remover tarefa">
+                                         </div>
+
+                                </li>`;
+
+   
+    contador++;
+
+}
+}
+
+//adiciona a função de criar o card ao botão '+'
+btnAddTarefa.addEventListener('click', function (evt) {
+
+    //impede de atualizar a página
+    evt.preventDefault();
+
+    //Chama a função que cria o card em tela
+    criaCard(tarefasPendentes);
+
+    //Limpando o campo de texto após salvar
+    inputText.value = "";
+
+});
+
+// evento para remover elemento de uma div e inserir em outra 
+// O evento acontece nessa lista, não no botão de submit
+tarefasPendentes.addEventListener('click', function (event) {
+
+    //seleciona e armazena o elemento pai (div tarefa) para que possamos realoca-la
+    let tarefa = event.target.parentNode;
+
+    //se alvo do evento for elemento de classe 'not-done'
+    //Ou seja, se <tarefa> é marcada como NOT-DONE, a condição é verdadeira
+    if (event.target.classList.contains('not-done')) {
+
+        //remove a div tarefa de 'tarefasPendentes'
+        tarefa.parentNode.removeChild(tarefa);
+
+        //realoca de acordo com o novo pai selecionado
+        tarefasConcluidas.appendChild(tarefa)
+    }
+
+    /* Capturamos o item <tarefa> que foi clicado no icone de deletar */
+    if (event.target.classList.contains('excluir')) {
+
+        //Verificação de segurança
+        let apagaTarefa = confirm("Você realmente deseja apagar esta tarefa ?");
+        if (apagaTarefa) {
+
+            //Definimos o carte <item> que será removido
+            tarefa = tarefa.parentNode;
+
+            //Removemos o card <item> da lista de 'tarefasPendentes'
+            tarefa.parentNode.removeChild(tarefa);
+        }
+    }
+});
--- js/scriptapi.js
+++ js/scriptapi.js
@@ -1,7 +1,6 @@
 //formatando os butões svg 
 let btn = document.querySelector('#adicionarTarefa');
-const tarefasPendentes = document.querySelector('#todo');
-const tarefasConcluidas = document.querySelector('#completed');
+
 
 
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
@@ -10,28 +9,28 @@ var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:x
     
 btn.addEventListener('click', (event) => {
   event.preventDefault();
-  tarefasPendentes.innerHTML = "";
 
-  // API do GitHub para pegar os repositórios de um certo usuário em formato json
-  let url = `https://jsonplaceholder.typicode.com/todos/`;
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+    for(let i = 0; i < json.length; i++){
+      let todo = json[i];
+      let li = document.createElement('li');
+      li.innerText = todo.title ;
+      document.querySelector('#todo').appendChild(li);
+    }
+  });
 
-  fetch(url) // comunicação assíncrona - envia um GET para a url
-    .then( resposta => resposta.json() ) // quando chegar a resposta, objetifique o dado para o formato json
-    .then( reposArray => { // quando o objeto json estiver pronto (ele é um array de repositorios!)
-      // varrer todo o array, utilizando o campo name de cada item do array (repositorio) para inserir na lista
-      reposArray.forEach( (repo) => tarefasPendentes.innerHTML += `<li> ${repo.title} </li>` );
+//função apgar tarefa
+function apagarTarefa() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  parent.removeChild(item);
 
-                               
-     })
-    .catch(error => console.log(`houve um erro! ${error}`)); // quando o json estiver pronto, mostre o array de repos no log
-    
-     //chamar a função addItemTodo com parametro repo.title
+}
 
-      addItemTodo(repo.title);
-     
-
-});
-
+//função completar tarefa
 function completarTarefa() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
@@ -45,39 +44,34 @@ function completarTarefa() {
 
 }
 
-// criar o item da lista
-function addItemTodo(repos) {
-  var list = document.querySelector('.todo');
-
-  var item = document.createElement('li');
-  item.innerText = `${repos}`;
-
+let lis = document.querySelectorAll('li');
 
   var buttons = document.createElement('div');
-  buttons.classList.add('buttons');
+    buttons.classList.add('buttons');
+  
+    console.log(buttons);
+  
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeSVG;
+  
+    console.log(remove);
+  
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+    complete.innerHTML = completeSVG;
+  
+  //evento click para remover tarefa
+  
+    remove.addEventListener('click', apagarTarefa);
+  
+    //evento click para completar tarefa
+    complete.addEventListener('click', completarTarefa); 
+  
+    
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
 
-  var remove = document.createElement('button');
-  remove.classList.add('remove');
-  remove.innerHTML = removeSVG;
 
-  var complete = document.createElement('button');
-  complete.classList.add('complete');
-  complete.innerHTML = completeSVG;
-
-//evento click para remover tarefa
-
-  remove.addEventListener('click', apagarTarefa);
-
-  //evento click para completar tarefa
-  complete.addEventListener('click', completarTarefa); 
-
-  buttons.appendChild(remove);
-  buttons.appendChild(complete);
-  item.appendChild(buttons);
-
-  list.insertBefore(item, list.childNodes[0]);
-
-}
-
-
+});
 
